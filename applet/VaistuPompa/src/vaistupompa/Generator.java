465e121cec6cdd27983c8012d19eaba29ed1c000
VytautasK <vytautaskasp@gmail.com>
2010-12-13 18:51:36 +0200

    Implemented some new classes.
    Signed-off-by: VytautasK <vytautaskasp@gmail.com>

---
+++ applet/VaistuPompa/src/vaistupompa/Generator.java
@@ -0,0 +1,95 @@
+package vaistupompa;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Vytautas
+ */
+public class Generator implements IGenerator {
+
+    // <editor-fold defaultstate="collapsed" desc="Private members">
+    private int _lambda; //vaistų išdavimo laiko limitas
+    private int _t; //pareikalavimų skaičius per valandą
+    private double _painConst;
+    private ArrayList<IGeneratorValue> _generatedValues = new ArrayList<IGeneratorValue>();
+    // </editor-fold>
+
+    public Generator(int lambda, int t)
+    {
+        _lambda = lambda;
+        _t = t;
+    }
+
+    public Generator()
+    {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generate methods">
+    public double generate(double x1) {
+        return this.generate(x1, Double.MAX_VALUE, Double.MAX_VALUE);
+    }
+
+    public double generate(double x1, double x2) {
+        return this.generate(x1, x2, Double.MAX_VALUE);
+    }
+
+    public double generate(double x1, double x2, double x3) {
+        double minVal = Math.min(x1, x2);
+        minVal = Math.min(minVal, x3);
+        
+        double generated = Double.MAX_VALUE;
+
+        if (_painConst >= minVal)
+	{
+            Random rnd = new Random();
+            double e = (rnd.nextInt(1000) + 1)/1000;
+
+            generated = (double)((1/_lambda)*Math.log(e));
+	}
+
+        GeneratorValue val = new GeneratorValue();
+        val.setTime(generated);
+        val.setX1(x1);
+        val.setX2(x2);
+        val.setX3(x3);
+
+        this._generatedValues.add(val);
+
+        return generated;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Set methods">
+    public void setLambda(int lambda) {
+        this._lambda = lambda;
+    }
+
+    public void setT(int t) {
+        this._t = t;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Get methods">
+    public int getLambda() {
+        return this._lambda;
+    }
+
+    public int getT() {
+        return this._t;
+    }
+
+    public ArrayList<IGeneratorValue> getGeneratedValues() {
+        return _generatedValues;
+    }
+
+    public void setPainConst(double painConst) {
+        this._painConst = painConst;
+    }
+
+    public double getPainConst() {
+        return this._painConst;
+    }
+    // </editor-fold>
+}
--- applet/VaistuPompa/src/vaistupompa/MainForm.java
+++ applet/VaistuPompa/src/vaistupompa/MainForm.java
@@ -7,6 +7,7 @@
 package vaistupompa;
 
 import java.awt.Color;
+import javax.swing.SpinnerNumberModel;
 
 /**
  *
@@ -18,28 +19,106 @@ public class MainForm extends javax.swing.JApplet {
     public void init() {
         initComponents();
         this.getContentPane().setBackground(Color.white);
+
+        SpinnerNumberModel model = new SpinnerNumberModel();
+        model.setMinimum(0);
+        model.setMaximum(18000);
+        model.setValue(1);
+        model.setStepSize(1);
+
+        SpinnerNumberModel model2 = new SpinnerNumberModel();
+        model2.setMinimum(0);
+        model2.setMaximum(18000);
+        model2.setValue(1);
+        model2.setStepSize(1);
+
+        SpinnerNumberModel model3 = new SpinnerNumberModel();
+        model3.setMinimum(0);
+        model3.setMaximum(18000);
+        model3.setValue(1);
+        model3.setStepSize(1);
+
+        this.jSpinnerT.setModel(model);
+        this.jSpinnerDeltaT.setModel(model2);
+        this.jSpinnerLambda.setModel(model3);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTextPane1 = new javax.swing.JTextPane();
-
-        jLabel1.setText("Some testing text or smth");
+        jPanelParameters = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jSpinnerDeltaT = new javax.swing.JSpinner();
+        jSpinnerT = new javax.swing.JSpinner();
+        jSpinnerLambda = new javax.swing.JSpinner();
 
         jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        jPanelParameters.setBackground(new java.awt.Color(255, 255, 255));
 
-        jScrollPane2.setViewportView(jTextPane1);
+        jLabel1.setFont(new java.awt.Font("Lucida Sans Unicode", 0, 12)); // NOI18N
+        jLabel1.setText("Vidutinis vaisto pareikalavimų skaičius per valandą:");
+
+        jLabel2.setFont(new java.awt.Font("Lucida Sans Unicode", 0, 12)); // NOI18N
+        jLabel2.setText("Vaistų išdavimo laiko limitas (sekundėmis):");
+
+        jLabel3.setFont(new java.awt.Font("Lucida Sans Unicode", 0, 12)); // NOI18N
+        jLabel3.setText("Delta t (sekundėmis):");
+
+        jLabel4.setFont(new java.awt.Font("Lucida Sans Unicode", 0, 12)); // NOI18N
+        jLabel4.setText("<html>[PKA] - kuriam laikui pompa blokuojama po vaistų išskyrimo<br>[iPKA] - per kiek laiko vaistas įšvirkščiamas</html>");
+
+        javax.swing.GroupLayout jPanelParametersLayout = new javax.swing.GroupLayout(jPanelParameters);
+        jPanelParameters.setLayout(jPanelParametersLayout);
+        jPanelParametersLayout.setHorizontalGroup(
+            jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelParametersLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelParametersLayout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelParametersLayout.createSequentialGroup()
+                        .addGroup(jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jSpinnerLambda, javax.swing.GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE)
+                            .addComponent(jSpinnerT, javax.swing.GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE)
+                            .addComponent(jSpinnerDeltaT, javax.swing.GroupLayout.DEFAULT_SIZE, 76, Short.MAX_VALUE))))
+                .addContainerGap())
+        );
+        jPanelParametersLayout.setVerticalGroup(
+            jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelParametersLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jSpinnerLambda, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jSpinnerT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanelParametersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jSpinnerDeltaT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(17, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -48,42 +127,39 @@ public class MainForm extends javax.swing.JApplet {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(120, 120, 120)
+                        .addGap(188, 188, 188)
                         .addComponent(jButton1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(26, 26, 26)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(71, 71, 71)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(207, Short.MAX_VALUE))
+                        .addContainerGap()
+                        .addComponent(jPanelParameters, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(102, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(37, 37, 37)
+                .addComponent(jPanelParameters, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(141, 141, 141)
                 .addComponent(jButton1)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(91, Short.MAX_VALUE))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanelParameters;
+    private javax.swing.JSpinner jSpinnerDeltaT;
+    private javax.swing.JSpinner jSpinnerLambda;
+    private javax.swing.JSpinner jSpinnerT;
     // End of variables declaration//GEN-END:variables
 
 }
