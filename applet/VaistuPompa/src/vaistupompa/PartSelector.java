d29b0172b51ab6d3d59920da4a65398e5dd7c0e3
Šarūnas Gliebus <ssharunas@yahoo.co.uk>
2010-12-19 13:32:33 +0200

    Pridėtas grfiko sample

---
+++ applet/VaistuPompa/src/vaistupompa/PartSelector.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * PartSelector.java
+ *
+ * Created on 2010-12-19, 13.25.16
+ */
+
+package vaistupompa;
+
+/**
+ *
+ * @author ssharunas
+ */
+public class PartSelector extends javax.swing.JPanel {
+
+    /** Creates new form PartSelector */
+    public PartSelector() {
+        initComponents();
+        //TODO: finish
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+}
